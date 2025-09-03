1886e7a4f708a895df8f47a0c2df8f57f164e2f9
Niranjanan T <niranjanant7@gmail.com>
2025-09-03 13:58:05 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Footer>
    
    with the

--- src/app/customize/[id]/page.tsx
+++ src/app/customize/[id]/page.tsx
@@ -67,9 +67,9 @@ export default function CustomizePage({ params }: { params: Promise<{ id: string
     const resolvedParams = React.use(params);
     
     return (
-        <div className="flex flex-col h-screen bg-background">
+        <div className="flex flex-col min-h-screen bg-background">
             <Header />
-            <main className="flex-grow overflow-hidden">
+            <main className="flex-grow">
                 <React.Suspense fallback={<p>Loading...</p>}>
                     <CustomizePageContent params={resolvedParams} />
                 </React.Suspense>
