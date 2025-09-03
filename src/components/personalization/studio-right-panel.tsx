fa1045fcdd6532cdb88a122437f551ba3f66a80b
Niranjanan T <niranjanant7@gmail.com>
2025-09-03 17:36:15 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <StudioCanvas prod

--- src/components/personalization/studio-right-panel.tsx
+++ src/components/personalization/studio-right-panel.tsx
@@ -29,7 +29,7 @@ export function StudioRightPanel({ product }: StudioRightPanelProps) {
                     <TabsTrigger value="clipart"><Smile /></TabsTrigger>
                 </TabsList>
             </div>
-            <ScrollArea className="flex-grow">
+            <ScrollArea className="flex-grow" style={{ overflowY: 'scroll' }}>
                 <TabsContent value="text" className="p-0 m-0">
                     <TextTool />
                 </TabsContent>
