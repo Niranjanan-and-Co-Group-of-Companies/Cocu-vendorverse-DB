dd69459d3299cf6123b00be6a0870f15ab0f75d2
Niranjanan T <niranjanant7@gmail.com>
2025-09-10 14:03:03 +0000

    Reduce a little more , this also looks big and is interepting the image,

--- src/components/layout/cart-preview.tsx
+++ src/components/layout/cart-preview.tsx
@@ -91,7 +91,7 @@ export function CartPreview() {
                                     <div className="relative shrink-0">
                                         <Image src={item.selectedVariant?.image || item.image} alt={item.name} width={64} height={64} className="rounded-md object-cover" />
                                         {item.displayPrice?.hasDiscount && (
-                                            <Badge variant="destructive" className="absolute top-1 left-1 text-[9px] px-1 py-0">
+                                            <Badge variant="destructive" className="absolute top-1 left-1 text-[8px] px-1 py-0">
                                                 {item.displayPrice.discountText}
                                             </Badge>
                                         )}
