84c542046f96dd32c4e5132b58caecc853914e5b
Niranjanan T <niranjanant7@gmail.com>
2025-09-12 17:21:43 +0000

    Try fixing this error: `Build Error: Module not found: Can't resolve '@/

--- src/components/corporate/layout.tsx
+++ src/components/corporate/layout.tsx
@@ -8,7 +8,6 @@ import { SidebarProvider, SidebarInset } from '@/components/ui/sidebar';
 import { CorporateSidebar } from '@/components/layout/corporate-sidebar';
 import { GstVerificationDialog } from '@/components/corporate/gst-verification-dialog';
 import { CorporateAccountProvider } from '@/hooks/use-corporate-account-store';
-import { ChatSafetyDialog } from '@/components/corporate/messages/chat-safety-dialog';
 
 export default function CorporateLayout({
   children,
@@ -29,7 +28,6 @@ export default function CorporateLayout({
             </div>
           </SidebarInset>
           <GstVerificationDialog />
-          <ChatSafetyDialog />
       </SidebarProvider>
     </CorporateAccountProvider>
   );
