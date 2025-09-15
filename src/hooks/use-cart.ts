cd6076b9b4d468da2ad07f255c88d0af9d795ae8
Niranjanan T <niranjanant7@gmail.com>
2025-09-15 04:45:27 +0000

    Try fixing this error: `Console Error: Only plain objects can be passed

--- src/hooks/use-cart.ts
+++ src/hooks/use-cart.ts
@@ -40,7 +40,7 @@ export const useCart = create(
         
         const existingItem = currentItems.find(item => item.cartItemId === cartItemId);
 
-        const plainProduct = makePlain(await serializeProduct(product));
+        const plainProduct = makePlain(product) as unknown as PlainProduct;
 
         if (existingItem) {
           const newQuantity = existingItem.quantity + quantity;
