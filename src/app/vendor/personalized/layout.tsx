8c1fc6dd271e21a1b64cfe09e4cbf8037575f6f6
Niranjanan T <niranjanant7@gmail.com>
2025-09-18 05:22:08 +0000

    Try fixing this error: `Runtime Error: Error: Module [project]/src/app/v

--- src/app/vendor/personalized/layout.tsx
+++ src/app/vendor/personalized/layout.tsx
@@ -1,18 +1,10 @@
-
-'use client';
 import * as React from 'react';
-import { VendorSidebarLayout } from '@/components/layout/vendor-sidebar-layout';
-import { TermsUpdateDialog } from '@/components/common/terms-update-dialog';
+import { PersonalizedVendorLayoutClient } from '@/components/layout/personalized-vendor-layout-client';
 
 export default function PersonalizedVendorLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return (
-    <VendorSidebarLayout>
-      {children}
-      <TermsUpdateDialog userType="vendor" />
-    </VendorSidebarLayout>
-  );
+  return <PersonalizedVendorLayoutClient>{children}</PersonalizedVendorLayoutClient>;
 }
